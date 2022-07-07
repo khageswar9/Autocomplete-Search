662abbe940549cde327a046bf81c597e81cef0ee
KHAGESWAR <88511621+khageswar9@users.noreply.github.com>
2022-07-08 01:24:11 +0530

    Update Usercard.js

--- src/component/Usercard.js
+++ src/component/Usercard.js
@@ -6,8 +6,8 @@ import { useContext } from 'react';
 function Usercard(obj) {
   const {setdetails} = useContext(Searchcontext)
   return (
-    <div className='usercard' key={obj.obj.id} >
-        <div onClick={()=>{setdetails(obj)}}>
+    <div className='usercard' key={obj.obj.id} onClick={()=>{setdetails(obj)}}>
+        <div>
             <Avatar alt="Remy Sharp" src={obj.obj.image} sx={{ width: 30, height: 30 }}/>
             <span className='name'><b>{obj.obj.name}</b></span>
         </div>
